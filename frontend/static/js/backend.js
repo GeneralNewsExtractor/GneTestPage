3baaee1d3526f61a384ad5bef92ade7cbd96d806
kingname <contact@kingname.info>
2020-03-04 12:34:21 +0800

    修复noise_node_list参数为空可能导致的问题

--- frontend/static/js/backend.js
+++ frontend/static/js/backend.js
@@ -16,6 +16,11 @@ var app = new Vue({
                 alert('HTML 不能为空！')
                 return
             }
+            if (this.noise_node_list.length <= 0){
+                noise_node_list = []
+            } else {
+                noise_node_list = this.noise_node_list.split('\n')
+            }
             axios.post('/extract', {
                 html: this.html,
                 title_xpath: this.title_xpath,
@@ -23,7 +28,7 @@ var app = new Vue({
                 publish_time_xpath: this.publish_time_xpath,
                 host: this.host,
                 with_body_html: this.with_body_html,
-                noise_node_list:this.noise_node_list.split('\n')
+                noise_node_list: noise_node_list
             }).then((response) => {
                     result = JSON.stringify(response.data, null, 2).replace('<', '&lt;').replace('>', '&gt;')
                     app.extract_result = result
