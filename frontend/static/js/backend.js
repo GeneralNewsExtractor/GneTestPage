57c5c96b7142d467a2e1b46d2ad8c2ace5d18b9f
kingname <greensouth@foxmail.com>
2020-09-09 22:36:02 +0800

    更新 js

--- frontend/static/js/backend.js
+++ frontend/static/js/backend.js
@@ -53,7 +53,7 @@ var app = new Vue({
                 alert('全自动智能提取即将上线，敬请期待。现在请填写列表中任一一项的标题或者 XPath。')
                 return
             }
-            axios.post('/extract', {
+            axios.post('/extract_list', {
                 html: this.html,
                 sample: this.list_sample,
             }).then((response) => {
